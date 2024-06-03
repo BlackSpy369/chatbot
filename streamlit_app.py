33b23c26b78a5ae7530c2e277b8341e4856d6c51
Vansh Sharma <blackspyyt369@gmail.com>
2024-06-03 15:00:25 +0000

    Basic setup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,28 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+from transformers import pipeline,AutoModelForSeq2SeqLM,AutoTokenizer
+import pandas as pd
+from datasets import load_dataset
 
-"""
-# Welcome to Streamlit!
+if "df" not in st.session_state:
+    st.session_state.df=None
+#sidebar
+with st.sidebar:
+    st.write("# 1. Input data")
+    st.write("## 1.1. Use hugging face datasets module")
+    dataset_name=st.text_input(label="Enter datasets Name")
+    split=st.text_input(label="Enter dataset split (optional) by default is train[:10000]")
+    if dataset_name:
+        raw_dataset=load_dataset(dataset_name,split=split if split else "train[:10000]" )
+        st.session_state.df=pd.DataFrame(raw_dataset)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    st.write("# 2. Select LLM")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+#Main body
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+st.title("Fine Tune a LLMs")
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+st.subheader("Input data",divider="rainbow")
+if st.session_state.df is not None:
+    st.dataframe(st.session_state.df)
+    
